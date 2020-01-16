87cb51de8fa837657d8a80b65104ba824033b5b5
Soeren Koestler <soeren.koestler@t-online.de>
2020-01-16 08:38:30 +0100

    Beste Änderung!!!11!

--- NOR/mainwindow.cpp
+++ NOR/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "QPushButton"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPushButton* hi = new QPushButton();
+    hi->setText("Ich bin hot!");
+    hi->show();
 }
 
 MainWindow::~MainWindow()

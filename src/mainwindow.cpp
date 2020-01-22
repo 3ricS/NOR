59a5059ec006e7535e365ed4134c29e3c8617989
Soeren Koestler <soeren.koestler@t-online.de>
2020-01-22 16:53:04 +0100

    Hinzufuegen einer oberen ToolBar

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,16 +5,35 @@
 #include <view/networkgraphics.h>
 #include <QListView>
 #include <QHBoxLayout>
+#include <QToolButton>
 
 MainWindow::MainWindow(Model* model, QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow), _model(model)
 {
     ui->setupUi(this);
     this->setWindowTitle("NOR - Network of Resistance");
-    this->resize(1800, 1000);
+    this->resize(1080,720);
 
     // setup ui
     _networkScene = new NetworkGraphics();
     ui->networkView->setScene(_networkScene);
+
+   QToolBar* _upperToolBar = new QToolBar("upper");
+    _upperToolBar->setIconSize(QSize(20, 20));
+    _upperToolBar->setOrientation(Qt::Horizontal);
+    _upperToolBar->setMovable(false);
+    addToolBar(_upperToolBar);
+
+    QToolButton* Dateibutton = new QToolButton();
+    Dateibutton->showMenu();
+    Dateibutton->setText("Datei");
+    Dateibutton->setSizeIncrement(50,20);
+    _upperToolBar->addWidget(Dateibutton);
+
+    QToolButton* Buildbutton = new QToolButton();
+    Buildbutton->showMenu();
+    Buildbutton->setText("Erstellen");
+    Buildbutton->setSizeIncrement(50,20);
+    _upperToolBar->addWidget(Buildbutton);
 
 
     // set connections
@@ -30,14 +49,16 @@ void MainWindow::dropped()
 
 void MainWindow::paintSampleLine()
 {
-    _networkScene->addConnection(_networkScene, 1630, 120, 30, 800);
+    _networkScene->addConnection(_networkScene, 330, 400, 430, 500);
     ui->networkView->setScene(_networkScene);
+    //Hide DrawButton
+    ui->drawButton->hide();
 }
 
 void MainWindow::paintSampleResistor()
 {
-    _networkScene->addResistor(_networkScene, 0, 800);
-    _networkScene->addResistor(_networkScene, 1600, 0);
+    _networkScene->addResistor(_networkScene, 300, 300);
+    _networkScene->addResistor(_networkScene, 400, 500);
     ui->networkView->setScene(_networkScene);
 }
 

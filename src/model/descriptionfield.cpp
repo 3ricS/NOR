4d3945c5455ae9da8371dcd654edf9ccca242b4d
Moritz <moritz.fichte@alice.de>
2020-02-21 13:28:58 +0100

    discriptionfield

--- src/model/descriptionfield.cpp
+++ src/model/descriptionfield.cpp
@@ -1,6 +1,13 @@
 #include "descriptionfield.h"
 
-DescriptionField::DescriptionField()
+DescriptionField::DescriptionField(int x, int y, int id)
+    : QGraphicsItem(nullptr),
+    _xPosition(x), _yPosition(y), _id(id)
 {
+}
 
+
+void DescriptionField::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->drawText(_xPosition, _yPosition, _text);
 }
--- src/model/descriptionfield.h
+++ src/model/descriptionfield.h
@@ -2,13 +2,27 @@
 #define DESCRIPTIONFIELD_H
 
 #include <QGraphicsItem>
+#include <QPainter>
 
 
 
 class DescriptionField : public QGraphicsItem
 {
 public:
-    DescriptionField();
+    DescriptionField(int x, int y, int id);
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)override;
+
+    QString text(void) const {return _text;}
+    void setText(const QString &text) {_text = text;}
+
+private:
+    void paintInformations(QPainter* painter);
+
+    int _xPosition;
+    int _yPosition;
+    int _id;
+    QString _text = "Hallo";
+
 };
 
 #endif // DESCRIPTIONFIELD_H
